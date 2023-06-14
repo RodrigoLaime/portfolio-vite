c2d9919ad5723ca92ab5c344ffe7881c4c8c00b7
Rodrigo Laime <rodrigolaime141@gmail.com>
2023-06-14 17:20:04 -0300

    text aboutme add style

--- src/home/Main.jsx
+++ src/home/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
     <main className='main'>
       <section className='container-intro-main' id='main-about'>
         <div>
-          <h2>Hello! I am Rodrigo Lime</h2>
+          <h2>Hello! I am Rodrigo Laime</h2>
           <h3>Web Developer</h3>
         </div>
       </section>
@@ -18,18 +18,18 @@ const Main = () => {
         </article>
 
         <article className='main-text--about'>
-          <h4>Who am i?</h4>
-          <h3>i'm Web developer</h3>
+          <h4>About me</h4>
+          <h3><span>Who</span> am i?</h3>
           {/* <p>Soy un apasionado desarrollador web con experiencia en la creación de soluciones digitales innovadoras, especializado en el diseño centrado en el usuario. Mi enfoque se basa en la creatividad, los detalles y la creación de experiencias web atractivas y de alto rendimiento.</p> */}
           <p>
             I am a passionate web developer with experience creating innovative digital solutions, specializing in user-centered design. My approach is based on creativity, details and creating high-performing and engaging web experiences.
           </p>
-          <p>we use creativity to get our clients</p>
+          {/* <p>we use creativity to get our clients</p>
           <p>what I do</p>
           <ul>
             <li></li>
           </ul>
-          <div></div>
+          <div></div> */}
         </article>
 
 
